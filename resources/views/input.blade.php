239c57a785f0157fef0e46356eca767cc308dc2f
ryfazrin <riansera515@gmail.com>
2021-07-04 14:20:33 +0800

    first commit

---
+++ resources/views/input.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Feedback Loop</title>
+        <link rel="stylesheet" href="/css/app.css" />
+    </head>
+    <body>
+        <div id="app">
+            <feedback-input></feedback-input>
+            {{-- <example-component></example-component> --}}
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
